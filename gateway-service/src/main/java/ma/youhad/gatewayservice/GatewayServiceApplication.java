bb3e577f2f20445df4b98c890c7754aef8c78607
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-11-19 00:21:01 +0100

    feat(gateway-service): implement Java-based routing using RouteLocator
    * Added programmatic gateway routing through a RouteLocator bean:
      - Route r1: forwards /customers/** to lb://CUSTOMER-SERVICE
      - Route r2: forwards /products/** to lb://INVENTORY-SERVICE

--- gateway-service/src/main/java/ma/youhad/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/ma/youhad/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package ma.youhad.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +13,11 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
+    RouteLocator gatewayRoutes (RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("r1",predicateSpec -> predicateSpec.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route("r2",predicateSpec -> predicateSpec.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+                .build();
+    }
 }
